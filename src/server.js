a1f604cd1bbfe2c62d8fa26f6402e30bb9be2db4
foxrain-05 <1skakaodowx@gmail.com>
2024-03-17 03:14:15 +0900

    Set up a basic Node.js server and apply WebSocket

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-console.log('Server is running');
+import express from "express";
+import path from 'path';
+import http from 'http';
+import { WebSocketServer } from "ws";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.resolve() + "/src/views");
+app.use("/public", express.static(path.resolve() + "/src/public"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/*", (req, res) => {
+    res.redirect("/");
+});
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+server.listen(3000, () => {
+    console.log("Server started on http://localhost:3000");
+});
